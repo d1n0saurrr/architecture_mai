b13c3f185ab09c88e6c3cffc40ebf5503c27c0c3
d1n054ur <mavlono.rb@gmail.com>
2023-04-12 20:33:23 +0300

    fixed db script

--- mariadb/main_script.sql
+++ mariadb/main_script.sql
@@ -13,7 +13,8 @@ create table user (
 
 insert into user (login, password, role, first_name, last_name, email, tel) values
 ('admin', '1234', 'admin', 'admin', 'admin', 'admin@admin.com', '+71234567890'),
-('test_user', '1234', 'user', 'Name1', 'Surname1', 'user@user.com', '+72345678901');
+('test_user', '1234', 'user', 'Name1', 'Surname1', 'user@user.com', '+72345678901'),
+('test_user2', '1234', 'user2', 'Name2', 'Surname2', 'user2@user2.com', '+73456789012');
 
 create table route (
     id int not null AUTO_INCREMENT,
@@ -26,10 +27,10 @@ create table route (
 
 insert into route (name, points, author_id)
     select
-        "To home",
+        'To home',
         '["Point 1", "Point 2"]',
         id
-    from user where login = 'user';
+    from user where login = 'test_user';
 
 create table trip (
     id int not null AUTO_INCREMENT,
@@ -46,8 +47,19 @@ create table trip (
 insert into trip (author_id, name, description, route_id, date)
     select
         author_id,
-        "From Point 1 to Point 2",
+        'From Point 1 to Point 2',
         "Hey there! I'm going from Point 1 to Point 2. Text me if you're too!",
         id,
         now()
     from route where name = 'To home';
+
+create table passengers (
+    id int not null AUTO_INCREMENT,
+    passenger_id int not null references user(id),
+    trip_id int not null references trip(id),
+    primary key (id)
+);
+
+insert into passengers (passenger_id, trip_id) values
+    ((select id from user where login = 'test_user2'),
+    (select t.id from trip t join user u on u.id = t.author_id where u.login = 'test_user'));
